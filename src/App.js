155dc6d53e4edd1a2089050462d693129141d947
Tanmay Paul <tanmaypaulofficial@gmail.com>
2023-12-18 14:20:55 +0530

    issue resolved for footer positioning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import ItemList from './components/itemList/ItemList';
-import ProductDetails from './components/productDetails/ProductDetails';
-import Cart from './components/cart/Cart';
-import Checkout from './components/checkout/Checkout';
-import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import ItemList from "./components/itemList/ItemList";
+import ProductDetails from "./components/productDetails/ProductDetails";
+import Cart from "./components/cart/Cart";
+import Checkout from "./components/checkout/Checkout";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
 
 const App = () => {
 
   return (
-      <Router>
+    <Router>
+      <div className="flex flex-col min-h-screen">
         <Navbar />
+        <div className="flex-grow">
           <Routes>
             <Route path="/" element={<ItemList />} />
             <Route path="/product/:id" element={<ProductDetails />} />
             <Route path="/cart" element={<Cart />} />
             <Route path="/checkout" element={<Checkout />} />
           </Routes>
-          <Footer />
-      </Router>
+        </div>
+        <Footer />
+      </div>
+    </Router>
   );
 };
 
--- src/components/cart/Cart.js
+++ src/components/cart/Cart.js
@@ -6,32 +6,34 @@ const Cart = () => {
   const { cart, removeFromCart, calculateTotal } = useAppContext();
 
   return (
-    <div className="mx-4">
-      <h2 className="text-3xl font-bold mb-4">Shopping Cart</h2>
-      <ul>
-        {cart.map((item) => (
-          <li
-            key={item.id}
-            className="border p-4 mb-4 flex justify-between items-center"
-          >
-            <div>
-              <h3 className="text-xl font-semibold mb-2">{item.name}</h3>
-              <p className="text-gray-600">${item.price.toFixed(2)} each</p>
-              <p className="text-gray-600">Quantity: {item.quantity}</p>
-            </div>
-            <button
-              className="bg-red-500 text-white px-4 py-2 rounded"
-              onClick={() => removeFromCart(item)}
+    <div className="flex flex-col justify-between h-full mx-4">
+      <div>
+        <h2 className="text-3xl font-bold mb-4">Shopping Cart</h2>
+        <ul>
+          {cart.map((item) => (
+            <li
+              key={item.id}
+              className="border p-4 mb-4 flex justify-between items-center"
             >
-              Remove
-            </button>
-          </li>
-        ))}
-      </ul>
+              <div>
+                <h3 className="text-xl font-semibold mb-2">{item.name}</h3>
+                <p className="text-gray-600">${item.price.toFixed(2)} each</p>
+                <p className="text-gray-600">Quantity: {item.quantity}</p>
+              </div>
+              <button
+                className="bg-red-500 text-white px-4 py-2 rounded"
+                onClick={() => removeFromCart(item)}
+              >
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
       <div className="flex justify-between">
         <p className="text-2xl font-bold mt-4">Total: ${calculateTotal()}</p>
         <Link to="/checkout">
-          <button className="bg-blue-500 text-white px-4 py-2 mt-4 rounded mr-2 hover:text-gray-300">
+          <button className="bg-blue-500 text-white px-4 py-2 my-4 rounded mr-2 hover:text-gray-300">
             Checkout
           </button>
         </Link>
